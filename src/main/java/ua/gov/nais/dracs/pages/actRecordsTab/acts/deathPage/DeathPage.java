8cd5eeb8afb5e8a884cc57bcc3d918b40f921b06
d.huhushkin <holderok@gmail.com>
2018-12-05 12:15:15 +0200

    modified DeathPage.java, DeathPageTabDeadTab.java, DeathPageTabGeneralTab.java

--- src/main/java/ua/gov/nais/dracs/pages/actRecordsTab/acts/deathPage/DeathPage.java
+++ src/main/java/ua/gov/nais/dracs/pages/actRecordsTab/acts/deathPage/DeathPage.java
@@ -10,28 +10,32 @@ public class DeathPage extends AbstractPage {
 
     @FindBy(id = "acRegister")
     private WebElement creatNewAct;
-    @FindBy (xpath = "(//span[text() = 'Зберегти'])[1]")
+    @FindBy(xpath = "(//span[text() = 'Зберегти'])[1]")
     private WebElement saveButton;
-    @FindBy (xpath = "//div[@id = 'confirm-message']/following-sibling::div/div/button/span[text() = 'OК']")
+    @FindBy(xpath = "//div[@id = 'confirm-message']/following-sibling::div/div/button/span[text() = 'OК']")
     private WebElement okButton;
-    @FindBy (xpath = "//div[@id = 'alert-message-info']/following::span[text() = 'OК']")
+    @FindBy(xpath = "//div[@id = 'alert-message-info']/following::span[text() = 'OК']")
     private WebElement okAlertButton;
-
-    private WebElement generalTab;
-
-    private WebElement informationAboutDocuments;
-    private WebElement notesTab;
-    private WebElement certificatesTab;
 
     public DeathPage(WebDriver driver) {
         super(driver);
     }
 
-    public DeathPageTabGeneralTab creatNewAct(){
+    /**
+     * .
+     * click on Create New Act button
+     *
+     * @return - DeathPageTabGeneralTab.class object
+     */
+    public DeathPageTabGeneralTab createNewAct() {
         action.clickOnElement(creatNewAct);
         return new DeathPageTabGeneralTab(driver);
     }
-    public void saveAct(){
+
+    /**
+     * save act record
+     */
+    public void saveAct() {
         action.clickOnElement(saveButton);
         action.clickOnElement(okButton);
         action.clickOnElement(okAlertButton);
