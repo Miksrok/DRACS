239e80c9a33b8efa6215671b850b9490d4f1d5e1
d.huhushkin <holderok@gmail.com>
2019-01-03 15:48:15 +0200

    new file: MarriageApplicationStepPage and modified: MarriagePaymentStepPage

---
+++ src/main/java/ua/gov/nais/dracs/pages/portal/marriage/MarriageApplicationStepPage.java
@@ -0,0 +1,10 @@
+package ua.gov.nais.dracs.pages.portal.marriage;
+
+import org.openqa.selenium.WebDriver;
+import ua.gov.nais.dracs.pages.AbstractPage;
+
+public class MarriageApplicationStepPage extends AbstractPage {
+    public MarriageApplicationStepPage(WebDriver driver) {
+        super(driver);
+    }
+}
